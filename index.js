32a83e86d259df439f0a3a3d982d9dc8d9709482
xiaochenghua <xch@choicesoft.com.cn>
2018-12-03 18:37:16 +0800

    暂提

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import TreeTable from './components/TreeTable/index';
-import OrderDetail from './components/OrderDetail/index';
+#!/usr/bin/env node
+const fs = require("fs");
 
-export default {
-    TreeTable,
-    OrderDetail,
-};
+const data = fs.readFileSync('./data.txt','utf-8');
+console.log(data);
+const modal = fs.readFileSync('./src/OrderDetail/index.jsx', 'utf-8');
+const result = modal.replace(/const orderColumns = \[\]/g, data);
+fs.writeFileSync("./data.jsx",result);
