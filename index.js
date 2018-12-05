d435d19ac1a9aef9f71a878a2299f131d14384fd
xiaochenghua <xch@choicesoft.com.cn>
2018-12-05 09:59:06 +0800

    顶单详情页简单生成实现，配置文件格式待明确

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv;
+const program = require('commander');
 const colors = require('colors');
-const { autoCrateOrderDetail } = require('./src/index');
+const shell = require('shelljs');
+const path = require('path');
+const dir = path.resolve(__dirname);
+const { downloadOrderDetail, autoCrateOrderDetail } = require(`${dir}/src/index`);
+const packageJson = require(`${dir}/package.json`);
 
-if (argv.type) {
-  switch(argv.type) {
+// 定义命令行命令
+program
+  .version(packageJson.version, '-v, --version')
+  .usage('[options] <file ...>')
+  .option('-t, --type <templatename>', 'Select a template')
+  .option('-d, --download <templatename>', 'domnload a template configuration file')
+  .option('-c, --config <file path>', 'Select a configuration file, like ./config.js')
+  .option('-h, --help', 'output usage information')
+  .parse(process.argv);
+
+// 下载相应模板配置文件
+if (program.download) {
+  switch (program.download) {
     case 'orderDetail':
-      autoCrateOrderDetail(argv.type);
+      downloadOrderDetail(program.download);
       break;
-    default: console.log(colors.red('未找到指定模板 --type'));
+    default: shell.echo(colors.red('未找到指定模板 --download <templatename>'));
+  }
+}
+
+// 使用模板生成文件
+if (program.type) {
+  switch (program.type) {
+    case 'orderDetail':
+      autoCrateOrderDetail(program.type, program.config);
+      break;
+    default: shell.echo(colors.red('未找到指定模板 --type <templatename>'));
   }
 }
--- src/template/OrderDetail/index.jsx
+++ src/template/OrderDetail/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { DetailTable } from 'hermes-react';
@@ -10,145 +9,7 @@ const Page = ({
   formData,
   onBack,
 }) => {
-  const state = {
-    0: '已提交未处理',
-    1: '生效未付款',
-    2: '已付款',
-    Y: '订单已生效',
-    N: '订单下发中',
-    3: '订单取消',
-    4: '申请退款',
-    5: '已退款',
-    6: '订单结账',
-    7: '已下单',
-    51: '骑手已接单',
-    52: '订单已送达',
-    54: '骑手取餐中',
-    55: '订单配送中',
-    56: '等待骑手接单',
-    57: '用户申请退单',
-    58: '用户取消申请退单',
-    59: '驳回申请退单',
-    60: '等待用户支付',
-    61: '百度物流下单失败',
-    '': '全部',
-  };
-  const TAKEOUTTYPE = {
-    ELEME: '饿了么',
-    MEITUAN: '美团',
-    meiTuanld: '美团录单',
-    '': '全部',
-  };
-  const orderColumns = [       // 菜品信息
-    {
-      title: '菜品名称',
-      dataIndex: 'FOODSNAME',
-      key: 'FOODSNAME',
-      width: '30%',
-    },
-    {
-      title: '数量',
-      dataIndex: 'FOODNUM',
-      key: 'FOODNUM',
-      width: '10%',
-    },
-    {
-      title: '单位',
-      dataIndex: 'UNIT',
-      key: 'UNIT',
-      width: '10%',
-    }, {
-      title: '总价',
-      dataIndex: 'TOTALPRICE',
-      key: 'TOTALPRICE',
-      width: '20%',
-    }, {
-      title: '附加项',
-      dataIndex: 'VADDITEMNAME',
-      key: 'VADDITEMNAME',
-      width: '30%',
-    },
-  ];
-
-  const orderInformation = [
-    {
-      label: '订单号',
-      value: formData.RESV,
-    },
-    {
-      label: '订单来源',
-      value: TAKEOUTTYPE[formData.ORDFROM],
-    },
-    {
-      label: '流水号',
-      value: formData.VORDERNO,
-    },
-    {
-      label: '订单状态',
-      value: state[formData.STATE],
-    },
-    {
-      label: '门店编码',
-      value: formData.FIRMCODE,
-    },
-    {
-      label: '所属门店',
-      value: formData.FIRMNAME,
-    },
-    {
-      label: '下单时间',
-      value: formData.ORDERTIME,
-    },
-    {
-      label: '送达时间',
-      value: formData.DELIVERYTIME,
-    },
-    {
-      label: '订单总金额',
-      value: formData.SUMPRICE,
-    },
-    {
-      label: '实付金额',
-      value: formData.PAYMONEY,
-    },
-    {
-      label: '客户名称',
-      value: formData.NAM,
-    },
-    {
-      label: '客户电话',
-      value: formData.CONTACT,
-    },
-    {
-      label: '发票抬头',
-      value: formData.VINVOICETITLE,
-    },
-    {
-      label: '备注',
-      value: formData.REMARK,
-    },
-    {
-      label: '送餐地址',
-      value: formData.RECEIVERADDR,
-    },
-    {
-      label: '调度员',
-      value: formData.VDELIVERLEADER || '--',
-    },
-    {
-      label: '调度员电话',
-      value: formData.VDELIVERLEADERTELE || '--',
-    },
-    {
-      label: '配送员',
-      value: formData.VDELIVER || '--',
-    },
-    {
-      label: '配送员电话',
-      value: formData.SHIPPERPHONE || '--',
-    },
-  ];
-
+  {{template}}
   const title = text => (<span
     style={{
       color: '#666',
@@ -160,37 +21,6 @@ const Page = ({
   >
     {text}
   </span>);
-  const packageCol = [
-    {
-      title: '菜品名称',
-      dataIndex: 'dishName',
-      key: 'dishName',
-      width: '30%',
-    },
-    {
-      title: '数量',
-      dataIndex: 'num',
-      key: 'num',
-      width: '10%',
-    },
-    {
-      title: '单位',
-      dataIndex: 'unit',
-      key: 'unit',
-      width: '10%',
-    }, {
-      title: '总价',
-      dataIndex: 'total',
-      key: 'total',
-      width: '20%',
-    }, {
-      title: '附加项',
-      dataIndex: 'additemName',
-      key: 'additemName',
-      width: '30%',
-      render: text => text || '---',
-    },
-  ];
   const tableProps = {
     expandedRowRender: record =>
     (record.ISPACKAGE === '1' ? <Table
